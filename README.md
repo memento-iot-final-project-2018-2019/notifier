ab525343c1cd142c847405e2a05570a49ccf6e08
jotaro-sama <36264038+jotaro-sama@users.noreply.github.com>
2019-05-10 01:36:50 +0200

    sender works

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# notifier
-Component sending a notification to the user's Twitter account when the door is opened
+# MEmento notifier
+Python-based component sending a notification to the user's Twitter account when the door is opened
+
+## Requirements
+To have the necessary dependencies, create a virtual environment, activate it and install the `python-twitter` package: 
+```
+pip install python-twitter
+```
---
+++ sender.py
@@ -0,0 +1,20 @@
+import twitter
+import json
+
+with open("credentials.json", "r") as creds_file:
+    creds = json.load(creds_file)
+    oauth_consumer_key = creds["oauth_consumer_key"]
+    oauth_token = creds["oauth_token"]
+    oauth_token_secret = creds["oauth_token_secret"]
+    oauth_consumer_secret = creds["oauth_consumer_secret"]
+
+    api = twitter.Api(consumer_key=oauth_consumer_key,
+                    consumer_secret=oauth_consumer_secret,
+                    access_token_key=oauth_token,
+                    access_token_secret=oauth_token_secret)
+
+    msg = input("Type the message to send: ")
+    username = input("Type the twitter handle of the user to send the message to: ")
+    send_msg = api.PostDirectMessage(msg, user_id=None, screen_name=username)
+
+    print(send_msg)
